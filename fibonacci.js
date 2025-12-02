dcb7f29434e28780631230d95d87f45376456629
dee-diaz <diazthediablo@yahoo.com>
2025-12-02 11:53:24 -0300

    Recursive fibonacci solution

--- fibonacci.js
+++ fibonacci.js
@@ -13,3 +13,18 @@ function fibs(num) {
 }
 
 console.log(fibs(8));
+
+// Recursion
+function fibsRec(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  let prevArray = fibsRec(num - 1);
+  let lastNum = prevArray[prevArray.length - 1];
+  let secondLastNum = prevArray[prevArray.length - 2];
+
+  return [...prevArray, lastNum + secondLastNum];
+}
+
+console.log(fibsRec(8));
